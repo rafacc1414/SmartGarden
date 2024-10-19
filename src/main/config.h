7b0a693ccd2d9f96bef78ad1447ccb151344c38f
Rafael Calderon <rafacc1414@gmail.com>
2024-10-19 22:48:42 +0200

    Send sensor information from client to the web server

--- src/main/config.h
+++ src/main/config.h
@@ -5,4 +5,17 @@ IPAddress ip(192, 168, 1, 200);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-char hostname[] = "ESP8266.calderon";
+const char *hostname = "ESP8266.calderon";
+
+// URL del servidor donde enviarás los datos
+// const char* serverUrl = "http://SmartGarden.com/api/data";
+const char *serverUrl = "http://192.168.1.42:5000/api/data";
+
+typedef struct sensorMessage
+{
+  int air_temperature;
+  int air_humidity;
+  int luminosity;
+  int soil_moisture_steel;
+  int soil_moisture;
+} SensorMessage;
--- src/main/main.ino
+++ src/main/main.ino
@@ -1,6 +1,8 @@
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoOTA.h>
 #include <ESP8266mDNS.h>
+
 #include <DHT11.h>
 
 #include "config.h"  // Replace with your network data
@@ -9,9 +11,9 @@
 #include "ESP8266_Utils_mDNS.hpp"
 #include "ESP8266_Utils_ADS.hpp"
 
-#define ANALOGPIN A0      // Analog Pin
-#define DHTPIN D4         // Digital Pin connected to the DHT sensor
-#define RELAY_PIN D7      // Relay Pin D7 for water motor
+#define ANALOGPIN A0  // Analog Pin
+#define DHTPIN D4     // Digital Pin connected to the DHT sensor
+#define RELAY_PIN D7  // Relay Pin D7 for water motor
 
 // Analag Sensors
 #define SOIL_MOSTURE_STEEL_SENSOR 0
@@ -34,18 +36,19 @@ void setup() {
   InitOTA();
 
   InitMDNS();
-  
+
   // ADS
   ads.setGain(GAIN_ONE);
   ads.begin();
 
   pinMode(RELAY_PIN, OUTPUT);     // Initialize the relay pin as output
   digitalWrite(RELAY_PIN, HIGH);  // Initially turn off the relay (assuming active-low relay)
-
 }
 
 void loop() {
   ArduinoOTA.handle();
+
+  SensorMessage msg{};
 
   // Read Temperature and humidity
   {
@@ -61,10 +64,14 @@ void loop() {
       // Print error message based on the error code.
       Serial.println(DHT11::getErrorString(result));
     }
+
+    // Store data
+    msg.air_temperature = temperature;
+    msg.air_humidity = humidity;
   }
 
+  // Read Analog Sensors
   {
-    // Read Analog Sensors
     int16_t values[4];
     if (!ads_read_all(&ads, values))
       Serial.println("ERROR: ADS115 could not read Analog Inputs");
@@ -83,7 +90,7 @@ void loop() {
     // 13253 Seco
     // 8860 Humedad
 
-    // Ideal tras riego ambos sensores juntos: 
+    // Ideal tras riego ambos sensores juntos:
     //   Soil mosture steel: 18500
     //   Soil mosture: 7400
     int soilMoistureSteelValue = values[SOIL_MOSTURE_STEEL_SENSOR];
@@ -101,13 +108,52 @@ void loop() {
     Serial.println("%");
 
     delay(2000);
+
+
+    // Store data
+    msg.luminosity = luminosity;
+    msg.soil_moisture = soilMoistureValue;
+    msg.soil_moisture_steel = soilMoistureSteelValue;
   }
 
+  // Send Data To Server
+  SendData2Server(&msg);
 }
 
 void BumpWater() {
-  digitalWrite(RELAY_PIN, LOW);  // Relay on
-  delay(2000);  // Pump runs for 2 seconds
+  digitalWrite(RELAY_PIN, LOW);   // Relay on
+  delay(2000);                    // Pump runs for 2 seconds
   digitalWrite(RELAY_PIN, HIGH);  // Relay off
 }
 
+void SendData2Server(const SensorMessage *msg) {
+
+  if (WiFi.status() != WL_CONNECTED)
+    return;
+
+  WiFiClient wifiClient;
+  HTTPClient http;
+
+  // Iniciar la conexión al servidor
+  http.begin(wifiClient, serverUrl);
+  http.addHeader("Content-Type", "application/json");  // Cabecera del tipo de contenido
+
+  // Crear el JSON con los datos
+  String jsonPayload = "{\"soil_moisture\":" + String(msg->soil_moisture) + ", \"soil_moisture_steel\":" + String(msg->soil_moisture_steel) + ", \"luminosity\":" + String(msg->luminosity) + ", \"temperature\":" + String(msg->air_temperature) + ", \"humidity\":" + String(msg->air_humidity) + "}";
+
+  Serial.println(jsonPayload);
+
+  // Enviar la solicitud POST
+  int httpResponseCode = http.POST(jsonPayload);
+
+  // Imprimir la respuesta del servidor
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+    Serial.println("Respuesta del servidor: " + response);
+  } else {
+    Serial.println("Error en la conexión: " + String(httpResponseCode));
+  }
+
+  // Finalizar la conexión
+  http.end();
+}
